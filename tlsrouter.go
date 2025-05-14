51623a8d154ed143f1fd31f07468cb9fae3f643c
AJ ONeal <aj@therootcompany.com>
2025-05-13 20:25:43 -0600

    feat: TLS Hello World - or ClientHello at least

--- tlsrouter.go
+++ tlsrouter.go
@@ -1,6 +1,11 @@
 package tlsrouter
 
-import "net"
+import (
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+)
 
 func Listen(addr string) error {
 	srv, err := net.Listen("tcp", addr)
@@ -10,8 +15,27 @@ func Listen(addr string) error {
 
 	for {
 		conn, err := srv.Accept()
-		if err == nil {
-			_ = conn.Close()
+		if err != nil {
+			log.Printf("Error: %#v", err)
+			continue
 		}
+		go proxy(conn)
+	}
+}
+
+func proxy(conn net.Conn) {
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	tlsConn := tls.Server(conn, &tls.Config{
+		GetConfigForClient: func(hello *tls.ClientHelloInfo) (*tls.Config, error) {
+			fmt.Printf("ServerName (SNI): %s\n", hello.ServerName)
+			fmt.Printf("SupportedProtos (ALPN): %s\n", hello.SupportedProtos)
+			return nil, fmt.Errorf("stop handshake")
+		},
+	})
+	if err := tlsConn.Handshake(); err != nil {
+		log.Printf("TLS handshake error: %v", err)
 	}
 }
